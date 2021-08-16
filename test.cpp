e10c16e84521532c30445bfb1a2e8d38afdaa39f
guojin <guojin@localhost.localdomain>
2021-08-17 01:40:33 +0800

    浮点数

--- test.cpp
+++ test.cpp
@@ -13,17 +13,6 @@ extern "C"
 int main(int argc, char* argv[])
 {
 	Manager *manager = new Manager(MAPX, MAPZ);
-	lua_State *L = luaL_newstate();
-	luaL_openlibs(L);
-
-	manager->enter(L, 1, 2, 3, 4);
-	manager->enter(L, 2, 3, 4, 4);
-
-	manager->move(L, 1, 3, 8);
-	manager->move(L, 2, 4, 8);
-
-	manager->leave(L, 1);
-	manager->leave(L, 2);
 	delete manager;
 
 	return 1;
